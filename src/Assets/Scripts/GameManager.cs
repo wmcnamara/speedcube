f09fcd92d1412c3c22b0c1f77445b0b75cee024c
Weston McNamara <westonfmcnamara@gmail.com>
2020-01-07 22:50:04 -0500

    Game restart UX change.
    Allows the player to restart the game by pressing any key on the keyboard.
    
    Simple but nice UX change.

--- src/Assets/Scripts/GameManager.cs
+++ src/Assets/Scripts/GameManager.cs
@@ -11,6 +11,9 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI endGameScoreText;
 
     private float score;
+
+    //Game is over.
+    private bool endGame;
     // Update is called once per frame
     void Update()
     {
@@ -23,13 +26,23 @@ public class GameManager : MonoBehaviour
             GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(0, 0.5f, 0);
             GameObject.FindGameObjectWithTag("Map").transform.position = new Vector3(0, 0, 0);
         }
+        if (Input.anyKeyDown && endGame)
+        {
+            GameObject.Find("MenuGUIFunctions").GetComponent<MenuGUIFunctions>().ReloadThisLevel();
+        }
     }
 
+    private void Start()
+    {
+        endGame = false;
+    }
     public void EndGame()
     {
         endGamePanel.SetActive(true);
         scoreText.gameObject.SetActive(false);
         endGameScoreText.text = "Final Score: " + (int)score;
         Time.timeScale = 0;
+        //If they press a key reload the scene.
+        endGame = true;
     }
 }
